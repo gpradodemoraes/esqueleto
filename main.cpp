88533a7993009d0a192aa7a7d7ff532e227b7226
Gabriel Prado de Moraes <gpradodemoraes@outlook.com>
2025-12-06 14:37:26 -0300

    estudo com unique_ptr

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <fmt/core.h>
+#include <memory>
+#include <vector>
+
+
+std::unique_ptr<std::vector<int>> create_vector() {
+	auto temp_vec = std::make_unique<std::vector<int>>();
+
+	temp_vec->push_back(1);
+	temp_vec->push_back(10);
+	temp_vec->push_back(100);
+
+	return temp_vec;
+}
 
 int main() {
 	fmt::println("Hello, World!");
+
+	auto v = create_vector();
+	for (auto i : *v) {
+		fmt::println("--> {}", i);
+	}
 	return 0;
 }
