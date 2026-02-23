25de65a9baf7aef9733b317c6e64b3c8f3d77658
Gabriel Prado de Moraes <gpradodemoraes@outlook.com>
2026-02-23 15:09:30 -0300

    deixando mais verboso

--- BS_threadpool.cpp
+++ BS_threadpool.cpp
@@ -11,19 +11,20 @@ int BSThreadpoolmain() {
 	BS::thread_pool pool(10);
 	std::vector<std::future<std::string>> futures;
 
-	int timemilis = 0;
+	int timemilis = 58 * 100;
 	try {
 		for (const auto &entry : fs::directory_iterator("C:\\Users\\gabriel.moraes")) {
 			if (entry.is_regular_file()) {
 				std::string filename = entry.path().string();
 				std::cout << "Pushing file " << filename << "\n";
 
-				timemilis += 50;
+				timemilis -= 100;
 				// Submit one task per file
-				futures.push_back(pool.submit_task([filename, timemilis]() -> std::string {
+				futures.push_back(pool.submit_task([filename, timemilis]() {
 					std::this_thread::sleep_for(std::chrono::milliseconds(timemilis));
 					// Do work and return a result
-					return "Processed: " + filename;
+					std::cout << "Processing " << filename << "\n";
+					return "(" + std::to_string(timemilis) + ") Processed : " + filename;
 				}));
 			}
 		}
@@ -33,8 +34,8 @@ int BSThreadpoolmain() {
 		return 1;
 	}
 
-	// --- Wait for all tasks to complete ---
-	// Collect results
+	std::cout << "Wait for all tasks to complete --- Collect results\n";
+
 	for (auto &f : futures) {
 		std::cout << f.get() << "\n";
 	}
