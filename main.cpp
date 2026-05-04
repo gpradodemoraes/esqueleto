97d8e377e113b6e6f4df739a2bbba2009af7ed1e
Gabriel Prado de Moraes <gpradodemoraes@outlook.com>
2026-05-04 13:23:37 -0300

    Testando o simdjson

--- main.cpp
+++ main.cpp
@@ -1,50 +1,31 @@
 #include <fmt/core.h>
-#include <memory>
-#include <vector>
+#include <simdjson.h>
 #include "compiled_with.h"
 #include "git_hash.h"
-#include "thread_test.hpp"
-
-std::unique_ptr<std::vector<std::vector<int>>> create_vector() {
-	auto temp_vec = std::vector<int>();
-
-	temp_vec.push_back(1);
-	temp_vec.push_back(10);
-	temp_vec.push_back(100);
-
-	auto temp_vec_2 = std::vector<int>();
-
-	temp_vec_2.push_back(2);
-	temp_vec_2.push_back(20);
-	temp_vec_2.push_back(200);
-
-	auto ret_val = std::make_unique<std::vector<std::vector<int>>>();
-	ret_val->push_back(std::move(temp_vec_2));
-	ret_val->push_back(std::move(temp_vec));
-
-	return ret_val;
-}
+#include <string>
 
 int main() {
 	fmt::println("Hello, World!");
 	fmt::println("Compiled With: {}", COMPILED_WITH);
 	fmt::println("Git: {} {}", GIT_REV, GIT_BRANCH);
-	std::unique_ptr<std::vector<std::vector<int>>> v = create_vector();
-	for (auto const &i : *v) {
-		for (auto &j : i) {
-			fmt::println("--> {}", j);
-		}
-		fmt::println("======");
-	}
-	fmt::println("");
-	while (!v->empty()) {
-		auto i = v->back();
-		for (auto j : i) fmt::println("--> {}", j);
-		v->pop_back();
-		fmt::println("======");
+
+	int id_documento_unico = 0;
+	const std::string response =
+		R"({"success":true,"message":"Registro Gravado com Sucesso.","id_prestacao_contas":19789,"idEdicaoPdc":54741,"id_documento_unico":3043355,"ano":"2026","id_item_pdc":[9671789,0]})";
+
+	simdjson::ondemand::parser parser;
+	simdjson::padded_string json(response);
+	simdjson::ondemand::document doc = parser.iterate(json);
+	simdjson::ondemand::object object = doc.get_object();
+
+	bool success = static_cast<bool>(object.find_field("success").get_bool());
+	std::string message;
+	object.find_field("message").get_string(message);
+	if (success) {
+		id_documento_unico = static_cast<int>(object.find_field("id_documento_unico").get_int64());
 	}
 
-	fmt::println("========== início threads ==========");
-	thread_main();
+	fmt::println("STATUS:{}; MESSAGE:{}; ID_DOCUMENTO_UNICO:{}", success,message,id_documento_unico);
+
 	return 0;
 }
